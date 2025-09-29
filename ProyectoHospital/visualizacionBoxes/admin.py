a2323d183f5d301a16488844d4a91b95a58a3d00
m1lvn <m.kurtec@udd.cl>
2025-09-29 01:42:06 -0300

    aasdsacd

--- ProyectoHospital/visualizacionBoxes/admin.py
+++ ProyectoHospital/visualizacionBoxes/admin.py
@@ -90,6 +90,6 @@ admin.site.index_title = "Administración del Sistema"
 # URLS API PARA GESTIÓN DE DATOS
 # ===============================
 
-# Boxes: https://55omss5p3c.execute-api.us-east-1.amazonaws.com/dev/api/boxes
-# Pasillos: https://55omss5p3c.execute-api.us-east-1.amazonaws.com/dev/api/pasillos
-# Agendas: https://55omss5p3c.execute-api.us-east-1.amazonaws.com/dev/api/agendas
+# Boxes: https://hrpik4srp3.execute-api.us-east-1.amazonaws.com/dev/api/boxes
+# Pasillos: https://hrpik4srp3.execute-api.us-east-1.amazonaws.com/dev/api/pasillos
+# Agendas: https://hrpik4srp3.execute-api.us-east-1.amazonaws.com/dev/api/agendas
